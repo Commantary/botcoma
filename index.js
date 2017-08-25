b8acf28a2f600cd0593ce70704192910ffb1aeff
Commantary <31055935+Commantary@users.noreply.github.com>
2017-08-25 15:44:34 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Mise en place des en tetes
-const discord = require('./discord.js');
+const discord = require('discord.js');
 const client = new discord.Client();
-const token = "MzQ2OTI1NTgwNDU5MDQ4OTYw.DHS6Ng.Wthay4iV4BNKMahxQyjVPHEN_H4"
+const token = "MzQ2OTI1NTgwNDU5MDQ4OTYw.DHS6Ng.Wthay4iV4BNKMahxQyjVPHEN_H4";
 var prefix = ('+');
 var mention = '<@346925580459048960>';
 var servercount = client.guilds.size;
@@ -9,7 +9,7 @@ var servercount = client.guilds.size;
 
 // On start le bot
 client.on("ready", () => {
-	client.user.setPresence({ game: { name: 'Prêt à l\'utilisation', type: 0 } });
+	client.user.setPresence({ game: { name: 'Le bot peut buger !', type: 0 } });
 	console.log ("-------------------------------------");
 	console.log('    [!] ComaBot connecté [!]');
 	console.log ("-------------------------------------");
@@ -77,5 +77,9 @@ kick(client);*/
 /* Partie Teste1 */
 const test1 = require('./module/test1.js');
 test1(client);
+
+/* Partie help2 */
+const help2 = require('./module/help2.js');
+help2(client);
 
 client.login(token)
